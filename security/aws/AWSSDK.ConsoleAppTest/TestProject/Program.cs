a21a449061becf3fb90f5f796163d1ac985ad27f
Daniel D'Angelo Resende Barros <ddangelorb@outlook.com>
2021-05-01 16:39:47 -0300

    minus security files

--- security/aws/AWSSDK.ConsoleAppTest/TestProject/Program.cs
+++ security/aws/AWSSDK.ConsoleAppTest/TestProject/Program.cs
@@ -22,10 +22,9 @@ namespace TestProject
         {
             try
             {
-                string awsAccessKeyID = @"AKIAJZYLJJCLIQ42SI5A";
-                string awsSecretAccessKey = @"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";
-                var streamName = @"cog4securitystream";
-                //var streamARN = @"arn:aws:kinesisvideo:us-west-2:415871004468:stream/cog4securitystream/1536873161816";
+                string awsAccessKeyID = @"";
+                string awsSecretAccessKey = @"";
+                var streamName = @"";
 
                 var video = new AmazonKinesisVideoClient(
                     awsAccessKeyID,
@@ -91,70 +90,5 @@ namespace TestProject
                 Console.WriteLine(e.ToString());
             }
         }
-        /*
-        private async Task SetStreamUriKinesisGetMediaAsync()
-        {
-            try
-            {
-                string awsAccessKeyID = "AKIAJZYLJJCLIQ42SI5A";
-                string awsSecretAccessKey = "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";
-
-                var videoClient = new AmazonKinesisVideoClient(
-                    awsAccessKeyID,
-                    awsSecretAccessKey,
-                    new AmazonKinesisVideoConfig()
-                    {
-                        RegionEndpoint = RegionEndpoint.USWest2
-                    }
-                );
-
-                var videoClientStreams = await videoClient.ListStreamsAsync(new ListStreamsRequest());
-
-                var videoMediaClient = new AmazonKinesisVideoMediaClient(
-                    awsAccessKeyID,
-                    awsSecretAccessKey,
-                    new AmazonKinesisVideoMediaConfig
-                    {
-                        ServiceURL = "https://kinesisvideo.us-west-2.amazonaws.com"
-                    }
-                );
-
-                var streamName = "cog4securitystream";
-                var streamARN = "arn:aws:kinesisvideo:us-west-2:415871004468:stream/cog4securitystream/1536830392188";
-                var mediaRequest = new GetMediaRequest()
-                {
-                    StreamName = streamName,
-                    StreamARN = streamARN,
-                    StartSelector = new StartSelector
-                    {
-                        StartSelectorType = StartSelectorType.EARLIEST
-                    }
-                };
-
-                var getMediaResponse = await videoMediaClient.GetMediaAsync(mediaRequest);
-                _videoStreamingUri = getMediaResponse.ToString();
-
-                /*foreach (var streamInfo in clientStreams.StreamInfoList)
-                {
-                    var mediaRequest = new GetMediaRequest()
-                    {
-                        StreamARN = streamInfo.StreamARN,
-                        StreamName = streamInfo.StreamName,
-                        StartSelector = new StartSelector
-                        {
-                            StartSelectorType = StartSelectorType.EARLIEST
-                        }
-                    };
-
-                    var getMediaResponse = await videoMediaClient.GetMediaAsync(mediaRequest);
-                    _videoStreamingUri = getMediaResponse.ToString();
-                }*/
-        /*}
-        catch (Exception e)
-        {
-            var sss = e.ToString();
-        }
-    }*/
-
     }
 }
